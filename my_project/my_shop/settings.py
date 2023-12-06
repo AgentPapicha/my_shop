4461e67647a12b360c51e1ac7b8c0107c54b4ec1
Pavel <mydak2109@gmail.com>
2023-12-06 16:24:12 +0300

    Added README for project, fixed an issue with Django loaddata

--- my_project/my_shop/settings.py
+++ my_project/my_shop/settings.py
@@ -113,39 +113,31 @@ WSGI_APPLICATION = 'my_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-DATABASES = {
-
-    'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'USER': 'shop_admin',
-            'PASSWORD': 'pasha2109',
-            'NAME': 'shop_database',
-            'PORT': '5432',
-        }
-
-}
-
-#for docker
+#
 # DATABASES = {
 #
 #     'default': {
 #             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': os.environ.get('POSTGRES_NAME'),
-#             'USER': os.environ.get('POSTGRES_USER'),
-#             'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#             'HOST': 'db',
-#             'PORT': 5432,
+#             'USER': 'shop_admin',
+#             'PASSWORD': 'pasha2109',
+#             'NAME': 'shop_database',
+#             'PORT': '5432',
 #         }
 #
 # }
+
+# for docker
+DATABASES = {
+
+    'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get('POSTGRES_NAME'),
+            'USER': os.environ.get('POSTGRES_USER'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+            'HOST': 'db',
+            'PORT': 5432,
+        }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
